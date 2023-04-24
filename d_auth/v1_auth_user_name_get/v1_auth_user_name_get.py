f8556bd785d270659cea75fb7bca4407be5006e0
0woodev <0woodev@gmail.com>
2023-04-24 14:27:37 +0900

    FIX: response body data 가 boolean 이 되도록 수정

--- d_auth/v1_auth_user_name_get/v1_auth_user_name_get.py
+++ d_auth/v1_auth_user_name_get/v1_auth_user_name_get.py
@@ -1,7 +1,9 @@
 import logging
+from http import HTTPStatus
 
-from common.const import ConstAWS
+from common.const import ConstAWS, ConstHttp
 from common.pg_util import pg_util
+from common.response_builder import build_fail_response, init_json_response, build_response
 from common.response_handler import ResponseHandler
 
 logging.basicConfig(level=logging.INFO)
@@ -19,9 +21,7 @@ def lambda_handler(event, context):
     '''
 
     user = list(pg_util.execute_query(query_select_user_by_name, (name,)))
-
     is_duplicate_name = len(user) != 0
 
-    return {
-        'is_duplicate_name': is_duplicate_name,
-    }
+    return is_duplicate_name
+
