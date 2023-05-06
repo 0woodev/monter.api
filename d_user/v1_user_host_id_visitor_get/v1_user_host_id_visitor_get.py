16357fb5783c756b54aef80c1d277a999a2a89ca
0woodev <0woodev@gmail.com>
2023-05-06 11:04:18 +0900

    FIX: queryparam 안넣으면 None 으로 넘어와서 발생한 문제 해결

--- d_user/v1_user_host_id_visitor_get/v1_user_host_id_visitor_get.py
+++ d_user/v1_user_host_id_visitor_get/v1_user_host_id_visitor_get.py
@@ -13,7 +13,11 @@ logger = logging.getLogger('api')
 
 @ResponseHandler.api
 def lambda_handler(event, context):
-    date: str = event.get(ConstAWS.QUERY_PARAMETER, {}).get('date', datetime.date.today().strftime('%Y-%m-%d'))
+    query_params = event.get(ConstAWS.QUERY_PARAMETER)
+    if query_params is None:
+        query_params = {'date': datetime.date.today().strftime('%Y-%m-%d')}
+
+    date: str = query_params.get('date')
     host_id: int = int(event[ConstAWS.PATH_PARAMETERS]['host_id'])
 
     count_visitors_in_specific_date = f'''
@@ -45,3 +49,30 @@ def lambda_handler(event, context):
         'day': day_visitors_count,
         'total': total_visitors_count
     }
+
+
+{'version': '1.0', 'resource': '/user/{host_id}/visitor', 'path': '/api/user/75/visitor', 'httpMethod': 'GET',
+ 'headers': {'Content-Length': '0', 'Host': '5g7vyrifn4.execute-api.ap-northeast-2.amazonaws.com',
+             'Postman-Token': '290150f7-7465-4a39-bf50-8546fbcb5c60', 'User-Agent': 'PostmanRuntime/7.32.2',
+             'X-Amzn-Trace-Id': 'Root=1-6455b43c-5b69d81645ffedd32adab7ab', 'X-Forwarded-For': '119.196.225.193',
+             'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https', 'accept': '*/*',
+             'accept-encoding': 'gzip, deflate, br'},
+ 'multiValueHeaders': {'Content-Length': ['0'], 'Host': ['5g7vyrifn4.execute-api.ap-northeast-2.amazonaws.com'],
+                       'Postman-Token': ['290150f7-7465-4a39-bf50-8546fbcb5c60'],
+                       'User-Agent': ['PostmanRuntime/7.32.2'],
+                       'X-Amzn-Trace-Id': ['Root=1-6455b43c-5b69d81645ffedd32adab7ab'],
+                       'X-Forwarded-For': ['119.196.225.193'], 'X-Forwarded-Port': ['443'],
+                       'X-Forwarded-Proto': ['https'], 'accept': ['*/*'], 'accept-encoding': ['gzip, deflate, br']},
+ 'queryStringParameters': None, 'multiValueQueryStringParameters': None,
+ 'requestContext': {'accountId': '308340476166', 'apiId': '5g7vyrifn4',
+                    'domainName': '5g7vyrifn4.execute-api.ap-northeast-2.amazonaws.com', 'domainPrefix': '5g7vyrifn4',
+                    'extendedRequestId': 'EekZhhO4IE0EPWA=', 'httpMethod': 'GET',
+                    'identity': {'accessKey': None, 'accountId': None, 'caller': None, 'cognitoAmr': None,
+                                 'cognitoAuthenticationProvider': None, 'cognitoAuthenticationType': None,
+                                 'cognitoIdentityId': None, 'cognitoIdentityPoolId': None, 'principalOrgId': None,
+                                 'sourceIp': '119.196.225.193', 'user': None, 'userAgent': 'PostmanRuntime/7.32.2',
+                                 'userArn': None}, 'path': '/api/user/75/visitor', 'protocol': 'HTTP/1.1',
+                    'requestId': 'EekZhhO4IE0EPWA=', 'requestTime': '06/May/2023:01:58:20 +0000',
+                    'requestTimeEpoch': 1683338300660, 'resourceId': 'GET /user/{host_id}/visitor',
+                    'resourcePath': '/user/{host_id}/visitor', 'stage': 'api'}, 'pathParameters': {'host_id': '75'},
+ 'stageVariables': {'developMode': 'dev', 'stage_name': 'api'}, 'body': None, 'isBase64Encoded': False}
